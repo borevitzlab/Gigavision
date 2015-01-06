0bd198c35f38e5b97f326f281f40c08a73dac351
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2015-01-06 15:24:18 +1100

    Correct mouse interaction for pan/tilt interaction with live image

--- ipcamcontrol_gui.py
+++ ipcamcontrol_gui.py
@@ -5,6 +5,8 @@ Created on Mon Nov 24 18:22:54 2014
 @author: chuong
 """
 
+from __future__ import absolute_import, division, print_function
+
 import sys
 import time
 from PyQt4 import QtGui, QtCore, uic
@@ -21,7 +23,9 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
         self.pushButtonStartCamera.clicked.connect(self.initCamera)
+        self.pushButtonStopCamera.clicked.connect(self.stopCamera)
         self.pushButtonStartPanTilt.clicked.connect(self.initPanTilt)
+        self.pushButtonStopPanTilt.clicked.connect(self.stopPanTilt)
         self.pushButtonLoadCameraConfigFile.clicked.connect(self.loadCameraConfig)
         self.PanPos = 0
         self.TiltPos = 0
@@ -37,7 +41,9 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
         with open(Filename, 'r') as ConfigFile:
             self.lineEditConfigFilename.setText(Filename)
             self.CamConfig = yaml.load(ConfigFile)
-            print(yaml.dump(self.CamConfig))
+            self.textEditMessages.append("Loaded {}:".format(Filename))
+            self.textEditMessages.append("----------")
+            self.textEditMessages.append(yaml.dump(self.CamConfig))
             if "IPVAL" in self.CamConfig.keys():
                 self.lineEditIPCamAddress.setText(self.CamConfig["IPVAL"])
             if "USERVAL" in self.CamConfig.keys():
@@ -83,7 +89,9 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
                 self.CamConfigUpdated[Key] = text
             else:
                 self.CamConfigUpdated[Key] = self.CamConfig[Key]
-        print(yaml.dump(self.CamConfigUpdated))
+        self.textEditMessages.append("Updated camera configs:")
+        self.textEditMessages.append("----------")
+        self.textEditMessages.append(yaml.dump(self.CamConfigUpdated))
 
     def initCamera(self):
         self.CameraIP = self.lineEditIPCamAddress.text()
@@ -124,6 +132,14 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
                      self.updateZoomFocusInfo)
         self.threadPool[len(self.threadPool)-1].start()
 
+    def stopCamera(self):
+        for i in range(len(self.threadPool)):
+            print(self.threadPool[i].Name)
+            if self.threadPool[i].Name == "CameraThread":
+                self.threadPool[i].stop()
+                del self.threadPool[i]
+                break
+
     def initPanTilt(self):
         self.PanTiltIP = self.lineEditPanTiltAddress.text()
         self.PanTiltUsername = self.lineEditPanTiltUsername.text()
@@ -131,7 +147,9 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
         self.PanTilt = PanTilt(self.PanTiltIP, self.PanTiltUsername,
                                self.PanTiltPassword)
         self.textEditMessages.append("Initialised pan-tilt.")
-        self.PanPos, self.TiltPos = self.PanTilt.getPanTiltPosition()
+        PanPosStr, TiltPosStr = self.PanTilt.getPanTiltPosition()
+        self.PanPosDesired = float(PanPosStr)
+        self.TiltPosDesired = float(TiltPosStr)
         self.updatePositions()
 
         # start polling pan-tilt values and show
@@ -141,8 +159,21 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
                      self.updatePanTiltInfo)
         self.threadPool[len(self.threadPool)-1].start()
 
+    def stopPanTilt(self):
+        for i in range(len(self.threadPool)):
+            print(self.threadPool[i].Name)
+            if self.threadPool[i].Name == "PanTiltThread":
+                self.threadPool[i].stop()
+                del self.threadPool[i]
+                break
+
     def updateImage(self):
-        Image = self.Camera.Image
+        Image = np.zeros_like(self.Camera.Image)
+        Image[:, :, :] = self.Camera.Image[:, :, :]
+        Image[100, :, :] = 255
+        Image[:, 100, :] = 255
+        Image[-100, :, :] = 255
+        Image[:, -100, :] = 255
         # Convert to RGB for QImage.
         if Image is not None:
             height, width, bytesPerComponent = Image.shape
@@ -176,54 +207,79 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
             self.close()
         elif Key == QtCore.Qt.DownArrow:
             self.PanTilt.panStep("down", 10)
+            event.accept()
         elif Key == QtCore.Qt.UpArrow:
             self.PanTilt.panStep("up", 10)
+            event.accept()
         elif Key == QtCore.Qt.LeftArrow:
             self.PanTilt.panStep("left", 10)
+            event.accept()
         elif Key == QtCore.Qt.RightArrow:
             self.PanTilt.panStep("right", 10)
+            event.accept()
         elif Key == QtCore.Qt.Key_PageDown:
             self.Camera.zoomStep("out", 50)
+            event.accept()
         elif Key == QtCore.Qt.Key_PageUp:
             self.Camera.zoomStep("in", 50)
-        else:
-            super().keyPressEvent(event)
+            event.accept()
 
     def mousePressEvent(self, event):
-        if event.button() == QtCore.Qt.LeftButton:
+        if event.button() == QtCore.Qt.RightButton:
             self.objectSelected = self.childAt(event.pos())
-            self.mousePressed = True
-            self.mouseReleased = False
+            if self.objectSelected == self.labelCurrentViewImage:
+                QtGui.QApplication.setOverrideCursor(
+                    QtGui.QCursor(QtCore.Qt.SizeAllCursor))
             self.mouseStartPos = self.labelCurrentViewImage.mapFromGlobal(
                 event.globalPos())
 
     def mouseReleaseEvent(self, event):
-        if event.button() == QtCore.Qt.LeftButton:
-            self.mouseReleased = True
+        if event.button() == QtCore.Qt.RightButton:
             self.mouseEndPos = self.labelCurrentViewImage.mapFromGlobal(
                 event.globalPos())
-        if self.mousePressed and self.mouseReleased and \
-                self.objectSelected == self.labelCurrentViewImage:
-            dx = self.mouseEndPos.x() - self.mouseStartPos.x()
-            dy = self.mouseEndPos.y() - self.mouseStartPos.y()
-            print("Need to pan/tilt camera {},{}".format(dx, dy))
-            self.mousePressed = False
-            self.mouseReleased = False
-            pan = self.HFoV*dx*self.Image.shape[1]/self.labelCurrentViewImage.Width
-            tilt = self.VFoV*dy*self.Image.shape[0]/self.labelCurrentViewImage.Height
-            self.PanTilt.setPanTiltPosition(pan, tilt)
+            if self.objectSelected == self.labelCurrentViewImage:
+                QtGui.QApplication.restoreOverrideCursor()
+                dx = self.mouseEndPos.x() - self.mouseStartPos.x()
+                dy = self.mouseEndPos.y() - self.mouseStartPos.y()
+                self.mousePressed = False
+                dp = self.HFoV*dx/self.labelCurrentViewImage.width()
+                dt = self.VFoV*dy/self.labelCurrentViewImage.height()
+                if dp == 0.0 and dt == 0.0:
+                    # pan/tilt one degree at a time if right clicked at edge
+                    x = self.mouseEndPos.x()/self.labelCurrentViewImage.width()
+                    y = self.mouseEndPos.y()/self.labelCurrentViewImage.height()
+                    x *= self.Camera.Image.shape[1]
+                    y *= self.Camera.Image.shape[0]
+                    if x <= 100:
+                        dp = 1
+                    elif x >= self.Camera.Image.shape[1]-100:
+                        dp = -1
+                    if y <= 100:
+                        dt = 1
+                    elif y >= self.Camera.Image.shape[0]-100:
+                        dt = -1
+                print("Pan/tilt camera {},{} degrees".format(dp, dt))
+                self.PanPosDesired = self.PanPosDesired - dp
+                self.TiltPosDesired = self.TiltPosDesired + dt
+                self.PanTilt.setPanTiltPosition(
+                    self.PanPosDesired, self.TiltPosDesired)
+
 
 class CameraThread(QtCore.QThread):
     def __init__(self, Camera):
         QtCore.QThread.__init__(self)
         self.Camera = Camera
         self.NoImages = 0
+        self.Name = "CameraThread"
+        self.stopped = False
+        self.mutex = QtCore.QMutex()
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        while True:
+        self.stopped = False
+        while not self.stopped:
             self.Camera.snapPhoto()
             self.emit(QtCore.SIGNAL('ImageSnapped()'))
             ZoomPos = self.Camera.getZoomPosition()
@@ -232,22 +288,36 @@ class CameraThread(QtCore.QThread):
                       "{},{}".format(ZoomPos, FocusPos))
         return
 
+    def stop(self):
+        print("Stop CameraThread")
+        with QtCore.QMutexLocker(self.mutex):
+            self.stopped = True
+
 
 class PanTiltThread(QtCore.QThread):
     def __init__(self, PanTil):
         QtCore.QThread.__init__(self)
         self.PanTil = PanTil
+        self.Name = "PanTiltThread"
+        self.stopped = False
+        self.mutex = QtCore.QMutex()
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        while True:
+        self.stopped = False
+        while not self.stopped:
             time.sleep(0.3)  # time delay between queries
             PanPos, TiltPos = self.PanTil.getPanTiltPosition()
             self.emit(QtCore.SIGNAL('PanTiltPos(QString)'),
                       "{},{}".format(PanPos, TiltPos))
         return
+
+    def stop(self):
+        print("Stop PanTiltThread")
+        with QtCore.QMutexLocker(self.mutex):
+            self.stopped = True
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass(None)
