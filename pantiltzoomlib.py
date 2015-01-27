d4ca56b27bf3166acb70627580329299b0bf26a9
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2015-01-28 10:53:25 +1100

    Add optional displacement measurement using OpenCV

--- pantiltzoomlib.py
+++ pantiltzoomlib.py
@@ -22,6 +22,119 @@ from skimage.color import rgb2gray
 from scipy.spatial.distance import hamming
 
 
+def drawMatchesOpenCV(img1, kp1, img2, kp2, matches):
+    """
+    Source: http://stackoverflow.com/questions/20259025/module-object-has-no-attribute-drawmatches-opencv-python
+    This function takes in two images with their associated
+    keypoints, as well as a list of DMatch data structure (matches)
+    that contains which keypoints matched in which images.
+
+    An image will be produced where a montage is shown with
+    the first image followed by the second image beside it.
+
+    Keypoints are delineated with circles, while lines are connected
+    between matching keypoints.
+
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
+    import cv2
+
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+    out = np.zeros((max([rows1, rows2]), cols1+cols2, 3), dtype='uint8')
+
+    # Place the first image to the left
+    out[:rows1, :cols1, :] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2, cols1:cols1+cols2, :] = np.dstack([img2, img2, img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1), int(y1)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x2)+cols1, int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1), int(y1)), (int(x2)+cols1, int(y2)),
+                 (255, 0, 0), 1)
+
+    return out
+
+
+def getDisplacementOpenCV(Image0, Image1):
+    import cv2
+
+    img1 = cv2.cvtColor(Image0, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(Image1, cv2.COLOR_BGR2GRAY)
+
+    # Create ORB detector with 1000 keypoints with a scaling pyramid factor
+    # of 1.2
+    orb = cv2.ORB(1000, 1.2)
+
+    # Detect keypoints
+    (kp1, des1) = orb.detectAndCompute(img1, None)
+    (kp2, des2) = orb.detectAndCompute(img2, None)
+
+    # Create matcher and do matching
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+
+    # Sort the matches based on distance.  Least distance
+    # is better
+    matches = sorted(matches, key=lambda val: val.distance)
+
+    # collect displacement from the first 10 matches
+    dxList = []
+    dyList = []
+    for mat in matches[:10]:
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
+        dxList.append(abs(x1 - x2))
+        dyList.append(abs(y1 - y2))
+
+    dxMedian = np.median(np.asarray(dxList, dtype=np.double))
+    dyMedian = np.median(np.asarray(dyList, dtype=np.double))
+
+#    img3 = drawMatchesOpenCV(img1, kp1, img2, kp2, matches[:10])
+#    WindowName = "Displacement = {}, {}".format(dxMedian, dyMedian)
+#    cv2.imshow(WindowName, img3)
+#    cv2.waitKey()
+
+    return dxMedian, dyMedian
+
+
 def getDisplacement(Image0, Image1):
     Image0Gray = rgb2gray(Image0)
     Image1Gray = rgb2gray(Image1)
@@ -589,6 +702,7 @@ class Panorama(object):
                 continue
             Image0 = ImagePanList[0]
             Image1 = ImagePanList[i]
+#            dx, dy = getDisplacementOpenCV(Image0, Image1)
             dx, dy = getDisplacement(Image0, Image1)
             if PanInc != 0:
                 CamHFoV = Image0.shape[1]*PanInc*i/dx
