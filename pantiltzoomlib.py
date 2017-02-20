e1f07ac54f0302de7d3a8240a6fe921741cdf76c
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2017-02-20 17:16:17 +1100

    Testing autodoc

--- pantiltzoomlib.py
+++ pantiltzoomlib.py
@@ -47,7 +47,7 @@ def draw_matches_opencv(img1, kp1, img2, kp2, matches):
     Keypoints are delineated with circles, while lines are connected between
     matching keypoints.
 
-    TESTING TESTING123
+    TESTING TESTING123dassds
 
     :param img1: grayscale image
     :type img1: np.ndarray
